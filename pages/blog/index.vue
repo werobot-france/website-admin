b4b44479388bd380c83a028aea5fd68151b636bf
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2018-12-28 16:28:37 +0100

    ajout du systeme de 'tri' en fonction des locales sur la page blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,20 @@
 <template>
   <v-container>
-    <v-btn block large color="success" to="/blog/create">Creer un nouvel article</v-btn>
     <v-layout row wrap align-center justify-center>
-    <v-flex xs6 v-for="post in posts" pa-2>
+    <v-flex xs3 pt-4>
+      <v-select
+        :items="locales"
+        label="Langue"
+        solo
+        color="primary"
+        v-model="locale"
+        >
+      </v-select>
+    </v-flex>
+    <v-flex xs9>
+    <v-btn block large color="success" to="/blog/create">Creer un nouvel article</v-btn>
+    </v-flex>
+    <v-flex xs6 v-for="post in posts" pa-2 v-show="post.locale == locale">
       <v-card style="margin-top:25px; min-height: 26em;" :to="'/blog/' + post.id" :key="post.id">
         <v-img class="white--text" height="200px" :src="post.image">
           <v-container fill-height fluid>
@@ -29,7 +41,9 @@
   export default {
     data: () => ({
       posts: [],
-      noPost: false
+      noPost: false,
+      locales: ["fr", "en"],
+      locale: "fr"
     }),
     created() {
       this.$store.commit('SET_TITLE', 'Blog')
