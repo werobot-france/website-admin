614f9a0f171f6c326ba6ca8d5d59dadbe278c6f3
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2018-12-29 01:27:08 +0100

    arrangeage de la page blog mais sans css

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,68 +1,39 @@
 <template>
   <v-container>
-    <v-layout row wrap align-center>
-    <v-flex md6 v-for="post in posts" pa-2 v-show="post.locale == locale" :key="post.id">
-      <v-card :to="'/blog/' + post.id" :key="post.id">
-        <v-img class="white--text" height="200px" :src="post.image" gradient="to bottom, rgba(0,0,0, .2), #424242" dark>
-          <v-container fill-height fluid>
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="headline">{{ post.title }}</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-img>
-        <v-card-title>
-          <div>
-            <span class="grey--text">{{ post.created_at }}</span><br>
-            <span>{{ post.description }}</span>
-          </div>
-        </v-card-title>
-      </v-card>
-    </v-flex>
-    <v-speed-dial
-      direction="top"
-      transition="scale-transition"
-      fixed
-      right
-      bottom
-      style="transform: translateY(-100px)"
-    >
-      <v-btn
-        slot="activator"
-        large
-        color="red"
-        fab
-      >
-      <v-icon>flag</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        medium
-        color="grey darken-3"
-        @click="locale='fr'"
-      >
-        FR
-      </v-btn>
-      <v-btn
-        fab
-        medium
-        color="grey darken-3"
-        @click="locale='en'"
-      >
-        EN
-      </v-btn>
-    </v-speed-dial>
-      <v-btn
-        large
-        color="success"
-        fab
-        fixed
-        right
-        bottom
-        to="/blog/create"
-      >
-      <v-icon>add</v-icon>
+    <v-layout row wrap justify-space-between align-content-space-between>
+      <v-flex md6 v-for="post in posts" pa-2 v-show="post.locale == locale" :key="post.id">
+        <v-card :to="'/blog/' + post.id" :key="post.id" classs="blog" hover height="24em">
+          <v-img class="white--text" height="200px" :src="post.image" gradient="to bottom, rgba(0,0,0, .2), #424242"
+            dark>
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">{{ post.title }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title>
+            <div>
+              <span class="grey--text">{{ post.created_at }}</span><br>
+              <span>{{ post.description }}</span>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-speed-dial direction="top" transition="scale-transition" fixed right bottom style="transform: translateY(-100px)">
+        <v-btn slot="activator" large color="red" fab>
+          <v-icon>flag</v-icon>
+        </v-btn>
+        <v-btn fab medium color="grey darken-3" @click="locale='fr'">
+          FR
+        </v-btn>
+        <v-btn fab medium color="grey darken-3" @click="locale='en'">
+          EN
+        </v-btn>
+      </v-speed-dial>
+      <v-btn large color="success" fab fixed right bottom to="/blog/create">
+        <v-icon>add</v-icon>
       </v-btn>
     </v-layout>
   </v-container>
