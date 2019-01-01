5fe492122db775657428880da5b2fc14998f786e
Le_Futuriste <spamfree@matthieubessat.fr>
2019-01-01 22:33:22 +0100

    feat: delete and view on image

--- components/ImageNavigation.vue
+++ components/ImageNavigation.vue
@@ -3,7 +3,7 @@
     <v-dialog v-model="enabled" max-width="500px">
       <v-card>
         <v-card-title>
-          Gallerie d'image
+          Galerie d'image
         </v-card-title>
         <v-card-text class="pt-0">
           <div class="text-xs-center mb-2" v-if="images.length > 0">
@@ -33,24 +33,43 @@
                 d-flex
               >
                 <v-card flat tile class="d-flex">
-                  <v-img
-                    @click="copyAndFinish(image)"
-                    style="cursor: pointer"
-                    :src="image.url"
-                    :lazy-src="image.url"
-                    aspect-ratio="1"
-                    class="grey lighten-2"
-                  >
-                    <v-layout
-                      slot="placeholder"
-                      fill-height
-                      align-center
-                      justify-center
-                      ma-0
+                  <v-menu absolute>
+                    <v-img
+                      slot="activator"
+                      style="cursor: pointer"
+                      :src="image.small"
+                      :lazy-src="image.small"
+                      aspect-ratio="1"
+                      class="grey lighten-2"
                     >
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-layout>
-                  </v-img>
+                      <v-layout
+                        slot="placeholder"
+                        fill-height
+                        align-center
+                        justify-center
+                        ma-0
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-layout>
+                    </v-img>
+                    <v-list>
+                      <v-list-tile
+                        @click="copyAndFinish(image)"
+                      >
+                        <v-list-tile-title>Use</v-list-tile-title>
+                      </v-list-tile>
+                      <v-list-tile
+                        @click="openViewDialog(image)"
+                      >
+                        <v-list-tile-title>Open</v-list-tile-title>
+                      </v-list-tile>
+                      <v-list-tile
+                        @click="openDeleteDialog(image)"
+                      >
+                        <v-list-tile-title>Delete</v-list-tile-title>
+                      </v-list-tile>
+                    </v-list>
+                  </v-menu>
                 </v-card>
               </v-flex>
             </v-layout>
@@ -59,6 +78,52 @@
         <v-card-actions>
           <v-spacer/>
           <v-btn flat color="error" @click="enabled = false">
+            Fermer
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="deleteDialog" max-width="500px">
+      <v-card>
+        <v-card-title>Voulez vous vraiment supprimer cette image ? (#{{ toDeleteImage.id }})</v-card-title>
+        <v-card-actions>
+          <v-btn flat @click="deleteDialog = false" color="success">
+            Non
+          </v-btn>
+          <v-spacer />
+          <v-btn flat @click="destroy()" color="error" :loading="$store.state.isLoading && $store.state.loadingType === 'button'">
+            Oui, supprimer
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="viewDialog" max-width="500px">
+      <v-card>
+        <v-card-title>{{ toViewImage.caption }} (#{{ toViewImage.id }})</v-card-title>
+        <v-card-text>
+          <v-img
+            slot="activator"
+            style="cursor: pointer"
+            :src="toViewImage.large"
+            :lazy-src="toViewImage.large"
+            aspect-ratio="1"
+            class="grey lighten-2"
+          >
+            <v-layout
+              slot="placeholder"
+              fill-height
+              align-center
+              justify-center
+              ma-0
+            >
+              <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+            </v-layout>
+          </v-img>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn flat @click="viewDialog = false" color="primary">
             Fermer
           </v-btn>
         </v-card-actions>
@@ -76,9 +141,21 @@
       totalPage: 0,
       perPage: 9,
       images: [],
-      imagePublicPath: ''
+      imagePublicPath: '',
+      deleteDialog: false,
+      toDeleteImage: {},
+      viewDialog: false,
+      toViewImage: {}
     }),
     methods: {
+      openDeleteDialog: function (image) {
+        this.deleteDialog = true
+        this.toDeleteImage = image
+      },
+      openViewDialog: function (image) {
+        this.viewDialog = true
+        this.toViewImage = image
+      },
       open: function () {
         this.fetchPhotos()
         this.enabled = true
@@ -92,17 +169,30 @@
           this.totalPage = response.data.data.pagination.total_page
           this.images = response.data.data.images
           this.images.map(image => {
-            image.url = this.imagePublicPath + '/' + image.id + '/25.' + image.extension
+            image.small = this.imagePublicPath + '/' + image.id + '/25.' + image.extension
+            image.medium = this.imagePublicPath + '/' + image.id + '/50.' + image.extension
+            image.large = this.imagePublicPath + '/' + image.id + '/75.' + image.extension
             return image
           })
         })
       },
       copyAndFinish: function (image) {
-        this.$clipboard(image.url.replace('/25.', '/50.'))
+        this.$clipboard(image.medium)
         this.enabled = false
         this.$store.commit('ADD_ALERT', {
           color: 'success',
           text: 'Copié dans le presse papier!'
+        })
+      },
+      destroy: function () {
+        this.$apitator.delete('/image/' + this.toDeleteImage.id, {withAuth: true, loadingType: 'button'}).then(() => {
+          this.deleteDialog = false
+          this.toDeleteImage = {}
+          this.fetchPhotos()
+          this.$store.commit('ADD_ALERT', {
+            color: 'success',
+            text: 'Image supprimé!'
+          })
         })
       }
     },
