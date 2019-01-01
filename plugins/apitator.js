b682576d293d89587257993f68f258c5be0ed22a
Le_Futuriste <spamfree@matthieubessat.fr>
2019-01-01 21:08:10 +0100

    fix: apitator endpoint

--- plugins/apitator.js
+++ plugins/apitator.js
@@ -2,6 +2,6 @@ import Apitator from 'vue-apitator'
 import Vue from 'vue'
 
 Vue.use(Apitator, {
-  baseUrl: process.env.API_ENDPOINT === undefined || process.env.API_ENDPOINT === null ? 'http://localhost:8000' : process.env.API_ENDPOINT,
+  baseUrl: process.env.API_ENDPOINT === undefined || process.env.API_ENDPOINT === null ? 'https://api.werobot.fr' : process.env.API_ENDPOINT,
   defaultToken: localStorage['password'] !== null ? localStorage['password'] : ''
 })
